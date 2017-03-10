c2bb9899b41cfa91bcbd89d6b33df767d40f1cb1
Aman Seth <aseth@cs.ualberta.ca>
2017-03-10 12:24:40 -0700

    sanitizing works, ive separated the entry number and message for ? and @, need to figure out how to use them

--- dependencies/clientConnectAndTalk/connectAndTalk.c
+++ dependencies/clientConnectAndTalk/connectAndTalk.c
@@ -42,12 +42,12 @@ void connect_and_talk() {
 			if (str[0] == '?') {
 				printf("? --> inquiry about n'th entry\n");
 				int len = strlen(str) - 2; 
-				printf("%d is length\n", len);
+				//printf("%d is length\n", len);
 				//char inquired_line[len-1];
 				int i;
 				for(i = 1; i < len; i++){
 					if(isdigit((int)str[i])>0){
-						printf("%d str is good so far\n", i);
+						//printf("%d str is good so far\n", i);
 					}
 					else{
 						printf("dont work, not a digit\n");
@@ -57,6 +57,7 @@ void connect_and_talk() {
 				}
 				char substring[len];
 				memcpy(substring, &str[1], len);
+
 				printf("the n value is %s \n",substring );
 			}
 
@@ -71,12 +72,12 @@ void connect_and_talk() {
 				int i;
 				for(i = 1; i < len; i++){
 					if(isdigit((int)str[i])>0){
-						printf("%d digit so far\n", i);
+						//printf("%d digit so far\n", i);
 					}
 					else if(str[i] == 'p'){
 						char substring[i];
 						memcpy(substring, &str[1], i-1);
-						printf("the n value is %s \n",substring );
+						//printf("the n value is %s \n",substring );
 						
 						int j = 0;
 						int k = i;
@@ -86,19 +87,22 @@ void connect_and_talk() {
 						}
 						char len_of_update[j-1];
 						memcpy(len_of_update, &str[i+1], j);
-						printf("the length of the update message is %s \n",len_of_update);
+						//printf("the length of the update message is %s \n",len_of_update);
 						
 						if(str[k+1] != '\n'){
 							check = 0;
-							printf("no new line after number\n");
+							printf("no new line after number, doesnt work\n");
 							break;
 						}
-///I DONT KNOW HOW TO USE THE NEWLINE THING. I NEED IT TO GET THE INFORMATION AFTER A NEWLINE BUT IT ENDS THIS FUNCTION. DO I CALL FGETS AGAIN??
 
 						else{
-							char message[len-(j+i)];
-							memcpy(message, &str[j+i], len-(j+i));
-							printf("the update message is %s \n",message);
+							fgets(str, sizeof(str), stdin);
+
+							printf("the n'th value is %s \n",substring );
+
+							printf("the length of the update message is %s \n",len_of_update);
+
+							printf("message is %s \n", str);
 						}
 
 						
