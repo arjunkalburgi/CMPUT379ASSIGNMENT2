1bdccee7e624365c11f0caaffd07943ba5c90c49
Aman Seth <aseth@cs.ualberta.ca>
2017-03-09 20:44:09 -0700

    attempt at sanitizing, need to fix newline char conditional, dont know how to do that yet

--- dependencies/clientConnectAndTalk/connectAndTalk.c
+++ dependencies/clientConnectAndTalk/connectAndTalk.c
@@ -36,13 +36,82 @@ void connect_and_talk() {
 	}
 	
 	while(1) {
-		fgets(str, sizeof(str), stdin);
+		int check = 1;
+		if(fgets(str, sizeof(str), stdin)){
+			
+			if (str[0] == '?') {
+				printf("? --> inquiry about n'th entry\n");
+				int len = strlen(str) - 2; 
+				printf("%d is length\n", len);
+				//char inquired_line[len-1];
+				int i;
+				for(i = 1; i < len; i++){
+					if(isdigit((int)str[i])>0){
+						printf("%d str is good so far\n", i);
+					}
+					else{
+						printf("dont work, not a digit\n");
+						check = 0;
+						break;
+					}
+				}
+				char substring[len];
+				memcpy(substring, &str[1], len);
+				printf("the n value is %s \n",substring );
+			}
 
-		if (str[0] == 'e') {
-			printf("TY\n");
-			close (s);
-			return; 
+			else if ((str[0] == 'e')&&(str[1]=='n')&&(str[2]=='d')) {
+				printf("TY\n");
+				close (s);
+				return; 
+			}
+			else if (str[0] == '@'){
+				printf("@ --> update n'th entry \n");
+				int len = strlen(str)-2;
+				int i;
+				for(i = 1; i < len; i++){
+					if(isdigit((int)str[i])>0){
+						printf("%d digit so far\n", i);
+					}
+					else if(str[i] == 'p'){
+						char substring[i];
+						memcpy(substring, &str[1], i-1);
+						printf("the n value is %s \n",substring );
+						
+						int j = 0;
+						int k = i;
+						while(isdigit((int)str[k+1])){
+							j++;
+							k++;
+						}
+						char len_of_update[j-1];
+						memcpy(len_of_update, &str[i+1], j);
+						printf("the length of the update message is %s \n",len_of_update);
+						
+						if(str[k+1] != '\n'){
+							check = 0;
+							printf("no new line after number\n");
+							break;
+						}
+///I DONT KNOW HOW TO USE THE NEWLINE THING. I NEED IT TO GET THE INFORMATION AFTER A NEWLINE BUT IT ENDS THIS FUNCTION. DO I CALL FGETS AGAIN??
+
+						else{
+							char message[len-(j+i)];
+							memcpy(message, &str[j+i], len-(j+i));
+							printf("the update message is %s \n",message);
+						}
+
+						
+					}
+				}
+
+			}
 		}
+		if(!check){
+			continue;
+		}
+
+		
 
 		// client sanitize (formatting)
 		do_crypt(str); // encrypt
